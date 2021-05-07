53183c11a2256fe20b544240fcb582dba4d13ff3
Gema <gemaakbar213@gmail.com>
2021-05-07 20:23:27 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+const {
+  addBook, getAllBook, getBookById, editBookById, deleteBook,
+} = require('./handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBook,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBook,
+  },
+  {
+    method: 'GET',
+    path: '/books/{id}',
+    handler: getBookById,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{id}',
+    handler: editBookById,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{id}',
+    handler: deleteBook,
+  },
+];
+
+module.exports = routes;
